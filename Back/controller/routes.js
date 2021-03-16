956b13878fc4f5658f3e4342025f6791dd962a45
celso <celso.sanchezviera@gmail.com>
2021-03-16 11:11:49 +0100

    add products, get products, rm producer entry, move products

--- Back/controller/routes.js
+++ Back/controller/routes.js
@@ -6,8 +6,11 @@ module.exports = function (app) {
     // app.delete('/tales', tales.delTale);
     
     //add product for a producer
-    app.put('/producer', product.addProduct);
-    app.get('/producer', product.getProducts);
+    app.put('/producer', product.addProducts);
+    app.get('/products', product.getProducts);
+    app.patch('/producer', product.moveProducts);
+    app.delete('/producer', product.rmProducerEntry);
+
     // app.get('/producer', product.getTales);
     // app.delete('/producer', product.delTales);
 }
