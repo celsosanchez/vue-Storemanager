80fae9e31c91953dbde233a77b1f592dae1bbd4c
celso <celso.sanchezviera@gmail.com>
2021-04-19 23:43:31 +0200

    front initiialized

---
+++ front/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-app>
+    <v-tabs
+    
+      v-model="tab"
+      background-color="transparent"
+      color="basil"
+      grow
+    >
+      <v-tab v-for="item in items" :key="item.name" :to="item.value">
+        {{ item.name }}
+      </v-tab>
+    </v-tabs>
+    <v-main>
+      <v-container fluid>
+        <router-view />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+<script>
+export default {
+  data: () => ({
+    tab: null,
+    items: {
+      Home: { name: "Home", value: "/" },
+      About: { name: "About", value: "/about" },
+      Producer: { name: "Producer", value: "/producer" },
+      Store: { name: "Store", value: "/store" },
+    },
+  }),
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ front/src/views/NotFound.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  <div class="col-full text-center"></div>
+  <h1 @click="asd">404</h1>
+  <h3>
+    Sorry the requested link {{$router.history.current.path}}  doesn't
+    exist
+  </h3> 
+  <router-link to="/">Go back Home </router-link>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      asdss: this.$router.path,
+    };
+  },
+  methods: {
+    asd() {
+      console.log(this.$router.history.current.path);
+    },
+  },
+};
+</script>
